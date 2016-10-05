7d7d55c235d06e6c38e757612912bb96d31fc821
Francois Michonneau <francois.michonneau@gmail.com>
2016-10-05 13:43:48 -0400

    generalize to species lists for multiple phyla

--- R/faunistic_lists.R
+++ R/faunistic_lists.R
@@ -1,8 +1,24 @@
-get_idigibio_records <- function(phylum = "Nemertea") {
+assemble_idigbio_records <- function(phyla = c("Echinodermata", "Nemertea", "Phoronida",
+                                               "Ctenophora", "Chaetognatha")) {
+    res <- lapply(phyla, get_idigbio_records)
+    names(res) <- phyla
+    res
+}
+
+get_idigbio_records <- function(phylum) {
+    message("Looking for ", phylum)
     res <- idig_search_records(rq = list(phylum = phylum, basisofrecord = "PreservedSpecimen",
+                                         scientificname = list(type = "exists"),
                                          geopoint = list(type = "geo_bounding_box",
                                                          top_left = list(lat = 51, lon = -133),
-                                                         bottom_right = list(lat = 23, lon = -51))))
+                                                         bottom_right = list(lat = 23, lon = -51))),
+                               limit = 10000)
+}
+
+assemble_idigbio_species_list <- function(idig_records) {
+    res <- lapply(idig_records, species_list_from_idigbio)
+    names(res) <- names(idig_records)
+    res
 }
 
 species_list_from_idigbio <- function(idig) {
@@ -21,6 +37,49 @@ species_list_from_idigbio <- function(idig) {
     res
 }
 
-get_bold_record_per_species <- function(sp) {
-NULL
+fetch_hook_bold_specimens_per_species <- function(key, namespace) {
+    res <- try(bold_specimens(taxon = paste0("'", key, "'")), silent = TRUE)
+    if (inherits(res, "try-error")) {
+        message("No record for ", key)
+        return(NULL)
+    }
+    message(nrow(res), " record(s) for ", key)
+    res
+}
+
+store_bold_specimens_per_species <- function(store_path = "data/bold_specimens_per_species") {
+    storr_external(driver_rds(store_path),
+                   fetch_hook_bold_specimens_per_species)
+}
+
+fill_bold_specimens_per_species <- function(sp_list, phylum, store = store_bold_specimens_per_species()) {
+    res <- lapply(sp_list, function(x) store$get(x, namespace = phylum))
+    invisible(res)
+}
+
+assemble_bold_specimens_per_species <- function(sp_lists) {
+    lapply(names(sp_lists), function(x) {
+        fill_bold_specimens_per_species(sp_lists[[x]], x)
+    })
+}
+
+make_table_records_per_species <- function(store = store_bold_specimens_per_species()) {
+    nmspc <- setdiff(store$list_namespaces(), "objects")
+    phyla <- nmspc
+    res <- lapply(phyla, function(p) {
+        sp <- st$list(namespace = p)
+        t <- lapply(sp, function(s) {
+            r <- st$get(s, namespace = p)
+            if (is.null(r))
+                n <- 0
+            else
+                n <- NROW(r)
+            data.frame(phylum = p,
+                       species = s,
+                       n_records = n,
+                       stringsAsFactors = FALSE)
+        })
+        bind_rows(t)
+    })
+    bind_rows(res)
 }
