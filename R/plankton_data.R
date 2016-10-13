d7af0344f0848c534b8d2d9a775c0adff4ff7116
Francois Michonneau <francois.michonneau@gmail.com>
2016-10-13 12:29:44 -0400

    calculate stats on plankton matches based on ESUs

--- R/plankton_data.R
+++ R/plankton_data.R
@@ -27,30 +27,52 @@ all_esus <- function(whitney_only = TRUE, phylum = "all") {
     unique(paste(sample_esu$phylum, sample_esu$group_esu, sep = "-"))
 }
 
+plankton_matches_binomial <- function(pk_id) {
+    pk_id %>%
+        filter(esu %in% all_esus()) %>%
+        ## we focus on matches identified at the species level
+        mutate(is_binomial = is_binomial(taxonomicidentification)) %>%
+        filter(is_binomial == TRUE)
+}
+
 prop_plankton_with_match <- function(pk_id) {
+    ## proportion of ESUs that have at least one match in
+    ## BOLD/GenBank
     pk_id <- pk_id %>% filter(esu %in% all_esus())
     length(unique(pk_id$esu))/length(all_esus())
 }
 
 prop_plankton_with_species_match <- function(pk_id) {
-    pk <- pk_id %>%
-        filter(esu %in% all_esus()) %>%
-        ## we focus on matches identified at the species level
-        mutate(is_binomial = is_binomial(taxonomicidentification)) %>%
-        filter(is_binomial == TRUE)
-    length(unique(pk$sequences))/length(labmanager::list_sequences())
+    ## proportion of ESUs that have a species-level match
+    pk <- plankton_matches_binomial(pk_id)
+    length(unique(pk$esu))/length(all_esus())
 }
 
-prop_plankton_with_multi_species_match <- function(pk_id) {
-    pk <- pk_id %>%
-        filter(esu %in% all_esus()) %>%
-        ## we focus on matches identified at the species level
-        mutate(is_binomial = is_binomial(taxonomicidentification)) %>%
-        filter(is_binomial == TRUE) %>%
-        group_by(sequences) %>%
-        summarize(
-            n_spp = length(unique(taxonomicidentification))
-        )
-    sum(pk$n_spp ==  1)/length(labmanager::list_sequences())
+prop_plankton_with_species_match_by_phylum <- function(pk_id) {
+    ## proportion of ESUs that have species-level match by phylum
+    ## first let's get the number of esu per phylum with matches
+    pk <- plankton_matches_binomial(pk_id) %>%
+        group_by(field_phylum) %>%
+        summarize(n_esu_match = length(unique(esu)))
+    ## then let's make a table of the total number of esu
+    esu <- vapply(pk$field_phylum, function(x)
+        length(all_esus(phylum = x)),
+        numeric(1))
+    names(esu) <- pk$field_phylum
+    esu <- data.frame(phylum=names(esu), n_esu_sampled = esu,
+                      stringsAsFactors = FALSE)
+    left_join(pk, esu, by = c("field_phylum" = "phylum")) %>%
+        mutate(p_esu_match = n_esu_match/n_esu_sampled)
+}
 
+
+prop_plankton_with_multi_species_match <- function(pk_id) {
+    ## proportion of ESUs that match to more than one binomial name
+    pk <- plankton_matches_binomial(pk_id) %>%
+        group_by(esu) %>%
+        summarize(
+            n_spp = length(unique(taxonomicidentification)),
+            spp = paste(unique(taxonomicidentification), collapse = ", ")
+        )
+    sum(pk$n_spp ==  1)/length(all_esus())
 }
