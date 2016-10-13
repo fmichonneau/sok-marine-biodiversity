a55aa43f60fb729054f8a2b55853b7f90ac4412f
Francois Michonneau <francois.michonneau@gmail.com>
2016-10-13 12:28:44 -0400

    improve is_binomial

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,19 @@
+cleanup_species_names <- function(nm) {
+    ## remove words that contain numbers
+    nm <- gsub("\\S*\\d\\S*", "", nm)
+    ## remove extra spaces
+    nm <- gsub("\\s{2, }", " ", nm)
+    ## remove sp(p).
+    nm <- gsub("\\sspp?\\.", "", nm)
+    ## remove words fully capitalized
+    nm <- gsub("\\s[A-Z]+", "", nm)
+    ## remove trailing spaces
+    nm <- gsub("\\s+$", "", nm)
+    nm
+}
+
+
 is_binomial <- function(nm) {
+    nm <- cleanup_species_names(nm)
     vapply(strsplit(nm, " "), function(x) length(x) == 2, logical(1))
 }
