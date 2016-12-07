3be2a095874f4b6b7876604ef5d2534b220db834
Francois Michonneau <francois.michonneau@gmail.com>
2016-12-07 18:24:49 -0500

    check overlap between faunistic list and obis records

--- R/obis.R
+++ R/obis.R
@@ -23,4 +23,20 @@ fetch_spp_from_obis <- function(wrm, feather_out) {
     res <- dplyr::bind_rows(res)
     feather::write_feather(res, feather_out)
 }
+
+spp_not_in_obis <- function(wrm, obis_feather) {
+    obis_qry <- feather::read_feather(obis_feather) %>%
+        dplyr::mutate(scientificName = tolower(scientificName)) %>%
+        dplyr::group_by(scientificName) %>%
+        dplyr::summarize(
+                   uuid_lst = first_5(id),
+                   n_obis = n()
+        )
+    res <- wrm %>%
+        dplyr::filter(is_marine == TRUE) %>%
+        dplyr::mutate(worms_valid_name = tolower(worms_valid_name)) %>%
+        dplyr::left_join(obis_qry, by = c("worms_valid_name" = "scientificName"))
+    stopifnot(all(res$is_marine))
+    stopifnot(all(res$is_binomial))
+    res
 }
