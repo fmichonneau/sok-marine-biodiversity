5e7541133b6b570b9c8e2421637911c41950f137
Francois Michonneau <francois.michonneau@gmail.com>
2016-12-10 10:30:30 -0500

    retry storr approach for caching is_in_eez status

--- R/usa_maps.R
+++ R/usa_maps.R
@@ -1,14 +1,37 @@
 get_map_usa <- function(file) {
     res <- geojsonio::geojson_read(x = file, what = "sp")
-    geojsonio::geojson_json(res)
+    res <- geojsonio::geojson_json(res)
+    saveRDS(res, file = "data/map_eez_usa.rds")
+    res
 }
 
 is_in_eez <- function(points, map_usa) {
-    message("Sit back, relax, it's going to be a while... (about 45 min)")
+    ##message("Sit back, relax, it's going to be a while...")
     pts <- geojsonio::geojson_json(points, lat = "lat", lon = "long")
     res <- lawn::lawn_within(pts, map_usa)
     res
 }
+
+eez_coords_hook <- function(key, namespace) {
+    coords <- unlist(strsplit(key, "\\|"))
+    pts <- data.frame(
+        coords = key,
+        lat = as.numeric(coords[1]),
+        long = as.numeric(coords[2]),
+        stringsAsFactors = FALSE
+    )
+    !is.null(is_in_eez(pts, readRDS("data/map_eez_usa.rds"))$features$properties)
+}
+
+eez_coords_store <- function(path = "data/eez_coords_storr") {
+    invisible(
+        storr::storr_external(
+                   storr::driver_rds(path),
+                   eez_coords_hook
+               )
+    )
+}
+
 
 is_in_eez_records <- function(filtered_data, map_usa, coords_store = eez_coords_store()) {
     p <- progress::progress_bar$new(total = nrow(filtered_data))
