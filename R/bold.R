b35ab3a980c3e241fa9b0e4a9b9c5d1363fe2a96
Francois Michonneau <francois.michonneau@gmail.com>
2017-02-17 14:28:49 -0500

    add code to get data from mammals + small BOLD refactor

--- R/bold.R
+++ R/bold.R
@@ -5,6 +5,27 @@ keep_marine_taxa <- function(worms) {
                       worms_valid_name != "not in worms") %>%
         dplyr::select(worms_valid_name) %>%
         unique
+    res
+}
+
+## this function takes a 1-column data frame, named with the content
+## of `col_nm`, and that contains the species names to look up in BOLD.
+## it returns a data frame.
+internal_add_bold <- function(res, col_nm) {
+    bold_rcrd <- bold_bin <- numeric(nrow(res))
+
+    for (i in seq_len(nrow(res))) {
+        bold <- store_bold_specimens_per_species()$get(tolower(res[[col_nm]][i]))
+        bold_rcrd[i] <- ifelse(is.null(bold) || inherits(bold, "character"),
+                               0, nrow(bold))
+        bold_bin[i] <- ifelse(is.null(bold) || inherits(bold, "character"),
+                              0, length(unique(na.omit(bold$bin_uri))))
+    }
+    res <- data.frame(worms_valid_name = res,
+                      n_bold_records = bold_rcrd,
+                      n_bins = bold_bin,
+                      stringsAsFactors = FALSE)
+    res
 }
 
 add_bold <- function(worms, use_worms = TRUE) {
@@ -19,19 +40,8 @@ add_bold <- function(worms, use_worms = TRUE) {
         col_nm <- "cleaned_scientificname"
     }
 
-    bold_rcrd <- bold_bin <- numeric(nrow(res))
+    res <- internal_add_bold(res, col_nm)
 
-    for (i in seq_len(nrow(res))) {
-        bold <- store_bold_specimens_per_species()$get(tolower(res[[col_nm]][i]))
-        bold_rcrd[i] <- ifelse(is.null(bold) || inherits(bold, "character"),
-                               0, nrow(bold))
-        bold_bin[i] <- ifelse(is.null(bold) || inherits(bold, "character"),
-                              0, length(unique(na.omit(bold$bin_uri))))
-    }
-    res <- data.frame(worms_valid_name = res,
-                      n_bold_records = bold_rcrd,
-                      n_bins = bold_bin,
-                      stringsAsFactors = FALSE)
     wrm <- dplyr::select_(worms, "rank", "taxon_name", col_nm) %>%
         unique %>%
         dplyr::filter_(.dots = lazyeval::interp(~ !is.na(var.), var. = as.name(col_nm)))
--- R/mammals.R
+++ R/mammals.R
@@ -31,3 +31,82 @@ get_accepted_itis_names <- function(nm) {
             NA_character_
     }, character(1))
 }
+
+read_asm_mammals <- function(file) {
+
+    lst_spp <- readr::read_csv(file) %>%
+        rename(common_name = `Common Name`,
+               order = Order,
+               family = Family,
+               raw_species_name = `Species Name`,
+               status = Status,
+               states = States,
+               distribution = Distribution,
+               notes = Notes,
+               zoonotic = Zoonotic,
+               disease = Disease,
+               publications = Publications) %>%
+        mutate(species_name = tolower(raw_species_name)) %>%
+        ## fixing typos
+        mutate(species_name = replace(species_name, species_name == "histrophoca fasciata", "histriophoca fasciata")) %>%
+        mutate(species_name = replace(species_name, species_name == "tamias quadirivittatus", "tamias quadrivittatus")) %>%
+        mutate(species_name = replace(species_name, species_name == "blarina penninsulae", "blarina peninsulae")) %>%
+        mutate(species_name = replace(species_name, species_name == "galucomys sabrinus", "glaucomys sabrinus")) %>%
+        ## remove extinct species
+        filter(species_name != "pteronotus pristinus")
+
+    ## other species with no matches:
+    ## chaetodipus rudinoris: only records are in Mexico
+    ## phenacomys ungava: only records are in Canada
+}
+
+fetch_mammals_from_idigbio <- function(mam) {
+    fields <- idigbio_fields()
+    split_species <- split_by_n(na.omit(mam$species_name), 10)
+    res <- lapply(split_species, function(x) {
+        qry <- list(scientificname = as.list(x),
+                    basisofrecord = "PreservedSpecimen"
+                    )
+        ridigbio::idig_search_records(rq = qry, fields = fields)
+    })
+    res <- dplyr::bind_rows(res) %>%
+        dplyr::distinct(uuid, .keep_all = TRUE) %>%
+        dplyr::rename(decimallatitude = geopoint.lat,
+                      decimallongitude = geopoint.lon)
+}
+
+
+asm_mammals_idigbio <- function(mam) {
+    res <- fetch_mammals_from_idigbio(mam)
+    res
+}
+
+add_bold_mammals <- function(mam_asm) {
+    mam_spp <- mam_asm %>%
+        dplyr::select(species_name) %>%
+        unique
+    res <- internal_add_bold(mam_spp, "species_name")
+    res
+}
+
+asm_mammals_idigbio_summary <- function(mam_asm, mam_idig) {
+
+    idig <- mam_idig %>%
+        filter(country == "united states") %>%
+        group_by(scientificname) %>%
+        tally() %>%
+        rename(n_idigbio = n)
+
+    asm <- mam_asm %>%
+        mutate(scientificname = tolower(species_name)) %>%
+        select(order, family, scientificname) %>%
+        left_join(idig, by = "scientificname")
+
+    asm
+}
+
+asm_summary <- function(mam_idig, mam_bold) {
+    mam_bold <- rename(mam_bold,
+                       scientificname = species_name)
+    dplyr::left_join(mam_idig, mam_bold, by = "scientificname")
+}
