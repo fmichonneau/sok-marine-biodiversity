5e5f63d97de88e754ba9592de67f3c5c6649441c
Francois Michonneau <francois.michonneau@gmail.com>
2017-03-29 17:32:40 -0400

    start adding iucn_red_list info

---
+++ R/red_list.R
@@ -0,0 +1,46 @@
+
+fetch_hook_red_list <- function(key, namespace) {
+    if (is.na(key)) return(NULL)
+    is_lower_case(key)
+    message("Getting info about ", key, appendLF = FALSE)
+    res <- try(rredlist::rl_search(name = key), silent = TRUE)
+    if (inherits(res, "try-error")) {
+        stop("ERROR")
+    }
+    message(" ... DONE.")
+    res
+}
+
+
+store_red_list <- function(store_path = "data/store_red_list") {
+    storr::storr_external(storr::driver_rds(store_path),
+                          fetch_hook_red_list)
+}
+
+get_iucn_info <- function(name) {
+    iucn_res <- store_red_list()$get(tolower(name))
+    if (!is.null(nrow(iucn_res))) {
+        tibble::tibble(
+                    worms_valid_name = iucn_res[["name"]][1],
+                    iucn_species_name = iucn_res[["result"]][1, "scientific_name"],
+                    iucn_marine_system = iucn_res[["result"]][1, "is_marine"],
+                    iucn_category = iucn_res[["result"]][1, "category"],
+                    iunc_criteria = iunc_res[["results"]][1, "criteria"]
+                )
+    } else {
+        tibble::tibble(
+                    worms_valid_name = iucn_res[["name"]][1],
+                    iucn_species_name = NA,
+                    iucn_marine_system = NA,
+                    iucn_category = NA,
+                    iunc_criteria = NA
+                )
+    }
+
+}
+
+add_iucn_status <- function(sp_list) {
+    name <- sp_list$worms_valid_name
+    res <- purrr::map_df(name, get_iucn_info)
+    dplyr::left_join(sp_list, res, by = "worms_valid_name")
+}
