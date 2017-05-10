7d0dc8f4099d26eb6fb12464486fb495139079c7
Francois Michonneau <francois.michonneau@gmail.com>
2017-05-10 17:14:09 -0400

    functionalize taxnomic data clean up

---
+++ R/data_cleanup.R
@@ -0,0 +1,26 @@
+chordata_classes_to_rm <- function()
+    c("actinopteri", "actinopterygii",
+      "cephalaspidomorphi", "agnatha",
+      "amphibia", "aves", "chondrichthyes",
+      "chondrichthys", "elasmobranchii",
+      "holocephali", "mammalia", "myxini",
+      "osteichthyes", "osteichthyes",
+      "petromyzonti", "pisces", "reptilia", "unknown")
+
+chordata_families_to_rm <- function()
+    c("anarhichantidae", "anthiidae",
+      "belontiidae", "branchiostegidae", "denticipitidae",
+      "doliolunidae", "eleotrididae", "fritillariidae",
+      "gobioididae", "grammistidae", "icelidae", "idiacanthidae",
+      "macrorhamphosidae", "zaniolepidae", "zaniolepididae")
+
+arthropod_classes_to_rm <- function()
+    c("arachnida", "myriapoda", "protura", "symphyla", "chilopoda",
+      "diplopoda", "hexapoda", "insecta", "trilobita", "unknown")
+
+gom_phyla <- function() {
+    taxa <- names(gom_taxa_to_keep())
+    taxa <- strsplit(taxa, "-")
+    taxa <- vapply(taxa, function(x) x[2], character(1))
+    tolower(unique(taxa))
+}
