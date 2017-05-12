90f093c10d120a8d33f5892c8e75ce17b1c2b147
Francois Michonneau <francois.michonneau@gmail.com>
2017-05-12 17:53:07 -0400

    use UpSetR to visualize intersection of datasets

--- R/comparison_by_geo.R
+++ R/comparison_by_geo.R
@@ -1,9 +1,9 @@
 compare_with_geo <- function(spp_list, geo_list) {
 
-    res <- list(
+    list(
         ## in spp_list but not in geo_list
         not_in_geo =
-            geo_list %>%
+            as_data_frame(geo_list) %>%
             dplyr::anti_join(spp_list, by = "worms_valid_name") %>%
             dplyr::filter(is_binomial == TRUE, is_marine == TRUE) %>%
             dplyr::distinct(worms_valid_name, .keep_all = TRUE) %>%
@@ -28,25 +28,39 @@ compare_with_geo <- function(spp_list, geo_list) {
             dplyr::distinct(worms_valid_name, .keep_all = TRUE) %>%
             dplyr::select(worms_id, worms_valid_name,
                           phylum = taxon_name)
-    )
+    ) %>% dplyr::bind_rows(.id = "data_source")
+}
 
-    res %>%
-        dplyr::bind_rows(.id = "data_source") %>%
-        dplyr::count(data_source, phylum) %>%
-        dplyr::filter(phylum %in% c("porifera", "platyhelminthes",
-                                    "nematoda", "mollusca", "echinodermata",
-                                    "cnidaria", "chordata", "bryozoa", "arthropoda",
-                                    "annelida")) %>%
-        tidyr::spread(data_source, n) %>%
-        dplyr::mutate(ymin_geo = -not_in_geo,
-                      ymin_list = 0,
-                      ymax_geo = geo_list - not_in_geo,
-                      ymax_list = spp_list) %>%
-        dplyr::select(phylum, starts_with("y")) %>%
-        tidyr::gather(xx, n_spp, -phylum) %>%
-        tidyr::separate(xx, into = c("ym", "list_orig"), sep = "_") %>%
-        tidyr::spread(ym, n_spp) %>%
-        ggplot(aes(x = reorder(phylum, ymax), ymin = ymin, ymax = ymax, colour = list_orig)) +
-        geom_linerange(size = 5, position = position_dodge(width = .5))+
-        coord_flip()
+generate_upsetr_csv <- function(..., file) {
+
+    has_bold_record <- function(worms_sp) {
+        r <- store_bold_specimens_per_species()$get(tolower(worms_sp))
+        if (is.null(r) || inherits(r, "character"))
+            0L
+        else nrow(r)
+    }
+
+    d <- list(...)
+    d <- lapply(d, function(x) {
+        if (exists("taxon_name", x))
+            rename_(x, "phylum" = "taxon_name")
+    })
+
+    dplyr::bind_rows(d, .id = "database") %>%
+        dplyr::mutate(phylum = tolower(phylum)) %>%
+        dplyr::count(phylum, worms_valid_name, database) %>%
+        tidyr::spread(database, n) %>%
+        dplyr::filter(!is.na(worms_valid_name)) %>%
+        dplyr::mutate(bold = has_bold_record(worms_valid_name)) %>%
+        dplyr::mutate_if(is.integer, funs(if_else(is.na(.), 0L, 1L))) %>%
+        readr::write_csv(path = file)
+
+}
+
+
+if (FALSE) {
+
+    upset(xx, order.by="freq")
+
+
 }
