e19989253347f999a0c3e4af4b8d65fefca866eb
Francois Michonneau <francois.michonneau@gmail.com>
2017-05-19 17:57:02 -0400

    first attempt at plot by phylum

--- R/comparison_by_geo.R
+++ R/comparison_by_geo.R
@@ -63,5 +63,103 @@ generate_upsetr_csv <- function(..., file) {
 
 plot_upsetr <- function(csv_file, ...) {
     readr::read_csv(csv_file) %>%
+        as.data.frame() %>%
         UpSetR::upset(order.by="freq", ...)
+}
+
+###
+
+if (FALSE) {
+
+    idig_gom <- compare_with_geo(gom_worms, idigbio_gom_records)
+    obis_gom <- compare_with_geo(gom_worms, obis_gom_records)
+
+    comp_gom <- dplyr::bind_rows(list(idigbio = idig_gom, obis = obis_gom),
+                                 .id = "database")
+
+    idig_koz <- compare_with_geo(kozloff_worms, idigbio_kozloff_records)
+    obis_koz <- compare_with_geo(kozloff_worms, obis_kozloff_records)
+
+    comp_koz <- dplyr::bind_rows(list(idigbio = idig_koz, obis = obis_koz),
+                                 .id = "database")
+
+    comp_db <- dplyr::bind_rows(list(gom = comp_gom,
+                                     kozloff = comp_koz), .id = "region") %>%
+        dplyr::filter(data_source %in% c("not_in_geo", "not_in_list"))
+
+    phyla_to_keep <- distinct(comp_db, region, phylum) %>%
+        count(phylum) %>%
+        filter(n == 2) %>%
+        .[["phylum"]]
+
+    comp_db <- filter(comp_db,
+                      phylum %in% phyla_to_keep)
+
+    rr <- comp_db %>%
+        count(region, database, data_source, phylum) %>%
+        mutate(number = ifelse(data_source == "not_in_geo", -n, n)) %>%
+        select(-n) %>%
+        #tidyr::spread(data_source, number) %>%
+        mutate(phylum = capitalize(phylum))
+
+    XX <- rr %>%
+        ungroup() %>%
+        arrange(number) %>%
+        distinct(phylum) %>%
+        .[[1]]
+
+    rr$phylum <- factor(rr$phylum, levels = XX)
+
+    pdf("figures/test_compare_dbs.pdf", height = 8, width = 15)
+    offset <- 160
+    dodge_width <- .6
+    ggplot(rr, aes(x = phylum,
+                   color = database)) +
+        geom_linerange(data = filter(rr, data_source == "not_in_geo"),
+                       aes(ymin = -offset, ymax = -offset + number),
+                       position = position_dodge(width = dodge_width)) +
+        geom_point(data = filter(rr, data_source == "not_in_geo"),
+                   aes(y = -offset + number), size = .7,
+                       position = position_dodge(width = dodge_width)) +
+        geom_linerange(data = filter(rr, data_source != "not_in_geo"),
+            aes(ymin = offset, ymax = offset + number),
+                       position = position_dodge(width = dodge_width)) +
+        geom_point(data = filter(rr, data_source != "not_in_geo"),
+                   aes(y = offset + number), size = .7,
+                       position = position_dodge(width = dodge_width)) +
+        geom_text(aes(x = phylum, label = phylum, y = 0),
+                  family = "Ubuntu Condensed",
+                  inherit.aes = FALSE,
+                  size = 4, color = "#5D646F") +
+        coord_flip() +
+        scale_y_continuous(breaks = c(seq(-1500, 0, by = 250) + -offset,
+                                      seq(0, 1500, by = 250) + offset),
+                           labels = c(abs(seq(-1500, 0, by = 250)), seq(0, 1500, by = 250))
+                           ) +
+        facet_wrap(~ region) +
+        theme_minimal(base_family = "Ubuntu Condensed") +
+        labs(
+            title = "compare db",
+            subtitle = "yo"
+        ) +
+        theme(text = element_text(color = "#3A3F4A"),
+              panel.grid.major.y = element_blank(),
+              panel.grid.minor = element_blank(),
+              panel.grid.major.x = element_line(linetype = "dotted", size = 0.1, color = "#3A3F4A"),
+              axis.title = element_blank(),
+              plot.title = element_text(face = "bold", size = 36, margin = margin(b = 10), hjust = 0.030),
+              plot.subtitle = element_text(size = 16, margin = margin(b = 20), hjust = 0.030),
+              plot.caption = element_text(size = 14, margin = margin(b = 10, t = 50), color = "#5D646F"),
+              axis.text.x = element_text(size = 12, color = "#5D646F"),
+              axis.text.y = element_blank(),
+              strip.text = element_text(color = "#5D646F", size = 18, face = "bold", hjust = 0.030),
+              plot.background = element_rect(fill = "#EFF2F4"),
+              plot.margin = unit(c(2, 2, 2, 2), "cm"),
+              legend.position = "none", #"""top",
+              legend.spacing = unit(0.01, "lines"),
+              legend.text  = element_text(family = "Ubuntu Condensed", size = 14),
+              legend.text.align = 0)
+        dev.off()
+
+
 }
