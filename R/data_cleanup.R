848f6ca523da73f320787dd5a13d64d4be8e61e0
Francois Michonneau <francois.michonneau@gmail.com>
2017-05-25 07:48:13 -0400

    add one fish family to list of taxa to ignore

--- R/data_cleanup.R
+++ R/data_cleanup.R
@@ -8,11 +8,11 @@ chordata_classes_to_rm <- function()
       "petromyzonti", "pisces", "reptilia", "unknown")
 
 chordata_families_to_rm <- function()
-    c("anarhichantidae", "anthiidae",
-      "belontiidae", "branchiostegidae", "denticipitidae",
-      "doliolunidae", "eleotrididae", "fritillariidae",
-      "gobioididae", "grammistidae", "icelidae", "idiacanthidae",
-      "macrorhamphosidae", "zaniolepidae", "zaniolepididae")
+    c("anarhichantidae", "anthiidae", "belontiidae",
+      "branchiostegidae", "denticipitidae", "doliolunidae",
+      "eleotrididae", "fritillariidae", "gobioididae", "grammistidae",
+      "icelidae", "idiacanthidae", "macrorhamphosidae", "pholididae",
+      "zaniolepidae", "zaniolepididae")
 
 arthropod_classes_to_rm <- function()
     c("arachnida", "myriapoda", "protura", "symphyla", "chilopoda",
