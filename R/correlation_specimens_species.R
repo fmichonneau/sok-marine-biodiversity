9b13bf01fb27965716c70873736a0d8991e51dde
Francois Michonneau <francois.michonneau@gmail.com>
2017-06-01 14:18:32 -0400

    add info about low number of records for rare phyla

--- R/correlation_specimens_species.R
+++ R/correlation_specimens_species.R
@@ -51,3 +51,18 @@ calc_prop_species_not_collected_since <- function(idig, max_year) {
         )
     format_output(nrow(dplyr::filter(res, last_collected < max_year))/nrow(res)*100)
 }
+
+calc_records_rare_phyla <- function(idig) {
+    res <- idig %>%
+        dplyr::count(clean_phylum, sort = TRUE) %>%
+        dplyr::mutate(is_rare = dplyr::if_else(n < 500, n, 0L))
+
+    list(
+        n_rare_phyla = dplyr::filter(res, is_rare > 0) %>% nrow(),
+        prop_rare_phyla = format_output((sum(res$is_rare)/sum(res$n))*100)
+    )
+}
+
+calc_n_idigbio_phyla <- function(idig) {
+    n_distinct(idig$clean_phylum)
+}
