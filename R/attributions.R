21e3652c0204c73f9bbee25d92ce189cc5c4b7d6
Francois Michonneau <francois.michonneau@gmail.com>
2017-06-09 12:40:25 -0400

    create code to get attributions from idigbio (progress towards #6)

---
+++ R/attributions.R
@@ -0,0 +1,39 @@
+## data storr for the iDigBio specimens by UUID. We need to get
+## individual records from the API, as if we tried to pool them first,
+## the attribution information is aggregated across the query and we
+## can't get the correspondance between the UUIDs for the specimen and
+## the collection that holds this record.
+fetch_hook_idigbio_by_uuid <- function(key, namespace) {
+    ridigbio::idig_search_records(rq = list(uuid = key), fields = idigbio_fields())
+}
+
+store_idigbio_uuid <- function(coords, store_path = "data/idigbio_uuid") {
+    storr::storr_external(storr::driver_rds(store_path),
+                          fetch_hook_idigbio_by_uuid)
+}
+
+
+## From a data frame of iDigBio records, creates a data frame that
+## contains the attribution information. We recalculate the item
+## counts, as there is not a good way to get this info from the raw
+## data downloaded during the geographic searches.
+idigbio_attribution <- function(idig) {
+    stopifnot(exists("uuid", idig))
+    map_df(idig$uuid, function(uu) {
+        rec <- store_idigbio_uuid()$get(uu)
+        attribution <- attr(rec, "attribution")
+        if (length(attribution) == 0L) return(NULL)
+        if (length(attribution) == 1L)
+        attribution[[1]] %>%
+            purrr::map_if(is.null,  ~ NA_character_) %>%
+            as_tibble %>%
+            ## remove nested info, not needed for our purposes
+            dplyr::select_if(is.atomic) %>%
+            dplyr::distinct()
+    }) %>%
+        dplyr::rename(recordset_uuid = uuid) %>%
+        dplyr::group_by(recordset_uuid) %>%
+        dplyr::mutate(itemCount = n()) %>%
+        dplyr::distinct()
+
+}
