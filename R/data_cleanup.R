f8707ef3276844f5347613740ef27a88f7568083
Francois Michonneau <francois.michonneau@gmail.com>
2017-06-20 19:05:38 -0400

    add remora to fish families to ignore

--- R/data_cleanup.R
+++ R/data_cleanup.R
@@ -9,7 +9,8 @@ chordata_classes_to_rm <- function()
 
 chordata_families_to_rm <- function()
     c("anarhichantidae", "anthiidae", "belontiidae",
-      "branchiostegidae", "denticipitidae", "doliolunidae",
+      "branchiostegidae", "denticipitidae", "doliolunidae", "echeneidae",
+      "echeneididae",
       "eleotrididae", "fritillariidae", "gobioididae", "grammistidae",
       "icelidae", "idiacanthidae", "macrorhamphosidae", "pholididae",
       "zaniolepidae", "zaniolepididae")
