1b8a6e82538aa9f809fb3938bf3d69e5de5c3537
François Michonneau <francois.michonneau@gmail.com>
2017-09-01 19:04:23 -0400

    simplify raster manipulation for heat maps

--- R/map_sampling.R
+++ R/map_sampling.R
@@ -3,30 +3,24 @@ us_raster <- function()
     raster(vals = NA, xmn = -130, ymn = 23, xmx = -61, ymx = 50, res = .2)
 
 
-data_map_samples <- function(recs) {
-    us_raster <- us_raster()
-    pts <- SpatialPoints(data.frame(lon = recs$decimallongitude,
-                                    lat = recs$decimallatitude))
-    r <- rasterize(pts, us_raster, fun = "count")
-    gg_r <- as.data.frame(as(r, "SpatialPixelsDataFrame"))
-    colnames(gg_r) <- c("value", "x", "y")
-    gg_r
-}
+data_map <- function(recs) {
 
-data_map_diversity <- function(recs) {
     us_raster <- us_raster()
-    raster_cell <- mapply(function(x, y) cellFromXY(us_raster, c(x, y)),
-                          recs$decimallongitude, recs$decimallatitude)
 
-    recs_r <- dplyr::bind_cols(recs, rastercell = raster_cell)
-    recs_spp <-  recs_r %>%
-        group_by(rastercell) %>%
-        summarize(
-            n_spp = n_distinct(worms_valid_name)
+    recs <- recs %>%
+        dplyr::mutate(rastercell = mapply(function(x, y)
+                          cellFromXY(us_raster, c(x, y)),
+                          decimallongitude, decimallatitude))
+
+    recs_r <- recs %>%
+        dplyr::group_by(rastercell) %>%
+        dplyr::summarize(
+            n_spp = n_distinct(worms_valid_name),
+            n_samples =  n()
         )
 
     ## not in use
-    h_index <- recs_r %>%
+    h_index <- recs %>%
         dplyr::select(phylum, worms_valid_name, rastercell) %>%
         dplyr::group_by(rastercell, worms_valid_name) %>%
         dplyr::summarize(n_records_per_spp = n()) %>%
@@ -36,13 +30,23 @@ data_map_diversity <- function(recs) {
         dplyr::group_by(rastercell) %>%
         dplyr::summarize(h_idx = - sum(p_i * log(p_i, 2)))
 
-    recs_r <- dplyr::full_join(recs_spp, h_index, by = "rastercell")
-    us_raster[na.omit(recs_r$rastercell)] <- recs_r$n_spp[!is.na(recs_r$rastercell)]
-    gg_r <- as.data.frame(as(us_raster, "SpatialPixelsDataFrame"))
-    colnames(gg_r) <- c("value", "x", "y")
-    gg_r
+    recs_r <- dplyr::full_join(recs_r, h_index, by = "rastercell")
+    as.data.frame(us_raster, xy = TRUE, na.rm = FALSE) %>%
+        dplyr::as_tibble() %>%
+        dplyr::mutate(rastercell = row_number()) %>%
+        dplyr::full_join(recs_r, by = "rastercell") %>%
+        dplyr::select(-layer)
 }
 
+data_map_diversity <- function(dm) {
+    dplyr::select(dm, x, y, n_spp) %>%
+        dplyr::rename(value = n_spp)
+}
+
+data_map_samples <- function(dm) {
+    dplyr::select(dm, x, y, n_samples) %>%
+        dplyr::rename(value = n_samples)
+}
 
 data_map_standardized_diversity <- function(sampling, diversity) {
     sampling <- sampling %>%
@@ -59,42 +63,39 @@ data_map_standardized_diversity <- function(sampling, diversity) {
 
 
 make_heatmap <- function(gg_r, title, limits = NULL) {
+
+    ## we use the world map to get Canda, Mexico and Caribbean islands
     state <- maps::map("world", fill = TRUE, plot = FALSE)
     ## convert the 'map' to something we can work with via geom_map
     IDs <- sapply(strsplit(state$names, ":"), function(x) x[1])
-    state <- map2SpatialPolygons(state, IDs=IDs, proj4string=CRS("+proj=longlat +datum=WGS84"))
+    state <- map2SpatialPolygons(state, IDs = IDs,
+                                 proj4string = CRS("+proj=longlat +datum=WGS84"))
 
-    us_bathy <- suppressMessages(getNOAA.bathy(lon1 = -128, lon2 = -60, lat1 = 22, lat2 = 51, keep = TRUE)) %>%
+    us_bathy <- suppressMessages(getNOAA.bathy(lon1 = -128, lon2 = -60,
+                                               lat1 = 22, lat2 = 51,
+                                               keep = TRUE)) %>%
         fortify() %>%
         filter(z < 0 & z > -1500)
 
     ## this does the magic for geom_map
     state_map <- fortify(state)
 
-    if (!is.null(limits)) {
-        limits <- c(1, limits)
-        mid_point <-  log(quantile(seq(min(gg_r$value),
-                                       max(limits),
-                                       by = 1), .02))
-    } else {
-        mid_point <-  log(quantile(seq(min(gg_r$value),
-                                       max(gg_r$value),
-                                       by = 1), .02))
-    }
+    mid_point <-  log(quantile(seq(min(gg_r$value, na.rm = TRUE),
+                                   max(gg_r$value, na.rm = TRUE),
+                                   by = 1), .02))
 
     ggplot() +
-        geom_raster(data = gg_r, aes(x = x, y = y, fill = value)) +
+        geom_raster(data = gg_r, aes(x = x, y = y, fill = value), na.rm = TRUE) +
         scale_fill_gradient2(low = "#5E98AE", mid = "#E3C94A", high = "#D5331E",
                              midpoint = mid_point,
                              breaks = c(1, 10, 100, 1000, 5000), trans = "log",
-                             limits = limits) +
+                             limits = limits, na.value = NA) +
         geom_map(data=state_map, map=state_map,
-                 aes(x=long, y=lat, map_id=id),
+                 aes(map_id=id),
                  fill="gray20", colour = "gray20", size = .05) +
         geom_contour(data = us_bathy, aes(x = x, y = y, z = z),
                      colour = "gray80", binwidth = 500, size = .1) +
         coord_quickmap(xlim = c(-128, -60), ylim = c(22, 51)) +
-        #scale_fill_viridis(trans = "log", breaks = c(1, 10, 100, 1000, 10000)) +
         theme_bw(base_family = "Ubuntu Condensed") +
         theme(legend.title = element_blank()) +
         ggtitle(title) +
