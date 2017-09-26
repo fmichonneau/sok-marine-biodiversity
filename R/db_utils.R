4bc01f9ec05751cb5805583e61f22e70312c10f4
François Michonneau <francois.michonneau@gmail.com>
2017-09-26 08:11:19 -0400

    use new syntax to connect to db

--- R/db_utils.R
+++ R/db_utils.R
@@ -1,6 +1,7 @@
 connect_sok_db <- function() {
     ## it's called idigbio but it's also used by OBIS
-    dplyr::src_postgres("idigbio", host = "localhost",
-                        user = "marinediversity",
-                        password = "password")
+    DBI::dbConnect(RPostgreSQL::PostgreSQL(),
+                   dbname = "idigbio", host = "localhost",
+                   user = "marinediversity",
+                   password = "password")
 }
