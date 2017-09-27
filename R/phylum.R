b5e845c8e8019a91d7b540483da1b5cd8452afc9
François Michonneau <francois.michonneau@gmail.com>
2017-09-27 12:16:11 -0400

    add code to deal with phyla

---
+++ R/phylum.R
@@ -0,0 +1,12 @@
+get_phyla_from_db <- function(db_table) {
+    sok_db %>%
+        tbl(db_table) %>%
+        dplyr::distinct(phylum) %>%
+        dplyr::arrange(phylum) %>%
+        dplyr::collect()
+}
+
+
+get_phyla <- function(csv) {
+    readr::read_csv(csv, col_types = "cc")
+}
