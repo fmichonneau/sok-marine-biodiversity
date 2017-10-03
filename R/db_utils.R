70aebd8f90252c43e49acccabfc3e86e1bfcdd8a
François Michonneau <francois.michonneau@gmail.com>
2017-10-03 11:05:11 -0400

    create pool DB only if it hasn't been created yet

--- R/db_utils.R
+++ R/db_utils.R
@@ -1,4 +1,6 @@
-## it's called idigbio but it's also used by OBIS
-sok_db <- pool::dbPool(drv = RPostgreSQL::PostgreSQL(), dbname = "idigbio",
-                       host = "localhost", user = "marinediversity",
-                       password = "password")
+## the database called idigbio but it's also used by OBIS
+if (!exists("sok_db")) {
+    sok_db <- pool::dbPool(drv = RPostgreSQL::PostgreSQL(), dbname = "idigbio",
+                           host = "localhost", user = "marinediversity",
+                           password = "password", idleTimeout = "Inf")
+}
