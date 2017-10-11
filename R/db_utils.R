f881ef3a36f193f044f4d9aa4725eca454f997d9
François Michonneau <francois.michonneau@gmail.com>
2017-10-11 17:12:51 -0400

    give up on pool to keep connection open

--- R/db_utils.R
+++ R/db_utils.R
@@ -1,6 +1,6 @@
 ## the database called idigbio but it's also used by OBIS
 if (!exists("sok_db")) {
-    sok_db <- pool::dbPool(drv = RPostgreSQL::PostgreSQL(), dbname = "idigbio",
-                           host = "localhost", user = "marinediversity",
-                           password = "password", idleTimeout = "Inf")
+    sok_db <- DBI::dbConnect(drv = RPostgreSQL::PostgreSQL(), dbname = "idigbio",
+                             host = "localhost", user = "marinediversity",
+                             password = "password")
 }
