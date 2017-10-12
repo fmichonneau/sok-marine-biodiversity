2c3ad68a9c316cff1ad69cb2fb03f52edb5212fd
François Michonneau <francois.michonneau@gmail.com>
2017-10-12 14:11:21 -0400

    fix typo in code comment

--- R/db_utils.R
+++ R/db_utils.R
@@ -1,4 +1,4 @@
-## the database called idigbio but it's also used by OBIS
+## the database is called idigbio but it's also used by OBIS
 if (!exists("sok_db")) {
     sok_db <- DBI::dbConnect(drv = RPostgreSQL::PostgreSQL(), dbname = "idigbio",
                              host = "localhost", user = "marinediversity",
