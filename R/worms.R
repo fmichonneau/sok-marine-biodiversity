063b94bd7f2038d549fa736f21beefdf82504911
François Michonneau <francois.michonneau@gmail.com>
2017-10-27 12:38:57 -0400

    move rescue_store to its own file

--- R/worms.R
+++ R/worms.R
@@ -127,22 +127,6 @@ worms_phylum_by_wid <- function(wid) {
     },  character(1))
 }
 
-
-rescue_store <- function(store) {
-    all_content <- store$list()
-    is_missing <- vapply(all_content,
-                         function(x) {
-        res <- try(store$get(x), silent = TRUE)
-        inherits(res, "try-error")
-    }, logical(1))
-    message(sum(is_missing), " records need to be rescued...")
-
-    to_redo <- all_content[is_missing]
-    sapply(to_redo, function(x) store$del(x))
-    sapply(to_redo, function(x) store$get(x))
-}
-
-
 worms_is_marine <- function(sp) {
     winfo <- store_worms_info()$get(sp)
     if (exists("isMarine", winfo)) {
