cf040aa80f1c35c39816c82672053feb14be6d2c
François Michonneau <francois.michonneau@gmail.com>
2021-10-08 11:19:57 +0200

    use auto core number detection

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,18 @@
-n_cores <- function() {
-  cmptr_name <- system("uname -n", intern = TRUE)
-  if (identical(cmptr_name, "francois-laptop") ||
-    identical(cmptr_name, "francois-XPS-15-9560")) {
-    options("mc.cores" = 8)
-  } else if (identical(cmptr_name, "ryanlab.whitney.ufl.edu")) {
-    options("mc.cores" = 12)
+n_cores <- function(auto = TRUE) {
+  if (auto) {
+    message("Using auto detection of the number of cores")
+    options("mc.cores" = parallel::detectCores())
   } else {
-    options("mc.cores" = 1)
-    message("no parallelization")
+    cmptr_name <- system("uname -n", intern = TRUE)
+    if (identical(cmptr_name, "francois-laptop") ||
+          identical(cmptr_name, "francois-XPS-15-9560")) {
+      options("mc.cores" = 8)
+    } else if (identical(cmptr_name, "ryanlab.whitney.ufl.edu")) {
+      options("mc.cores" = 12)
+    } else {
+      options("mc.cores" = 1)
+      message("no parallelization")
+    }
   }
   message("number of cores ", getOption("mc.cores"))
 }
