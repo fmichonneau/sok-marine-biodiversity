0867d34309e1094634de1a26e9d6ffd4c8ed4f7a
François Michonneau <francois.michonneau@gmail.com>
2021-10-23 10:56:46 -0400

    set up db in script

--- singularity_script.sh
+++ singularity_script.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-/etc/init.d/postgresql start
+pg_createcluster 12 main
+pg_ctlcluster 12 main start
+psql -d sok -c "CREATE EXTENSION postgis;"
+
 Rscript -e "setwd('/sok'); remake::make()"
