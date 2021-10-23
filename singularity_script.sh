d51fd50acdcfcb303a58de27d163820538785eb8
François Michonneau <francois.michonneau@gmail.com>
2021-10-23 11:34:39 -0400

    try to figure out what's happening

--- singularity_script.sh
+++ singularity_script.sh
@@ -4,4 +4,10 @@ pg_createcluster 12 main
 pg_ctlcluster 12 main start
 psql -d sok -c "CREATE EXTENSION postgis;"
 
+pg_ctlcluster 12 main restart
+
+pg_ctlcluster status
+
+Rscript -e "con = DBI::dbConnect(RPostgres::Postgres(), dbname='sok', user='rstudio'); con"
+
 Rscript -e "setwd('/sok'); remake::make()"
