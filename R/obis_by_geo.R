9be0f665b959302b86eb22e162e307cb904f5321
François Michonneau <francois.michonneau@gmail.com>
2021-10-30 16:49:18 -0400

    try to debug

--- R/obis_by_geo.R
+++ R/obis_by_geo.R
@@ -56,13 +56,15 @@ store_obis_by_geo <- function(coords, store_path = "data/storr_obis_by_geo") {
 internal_fill_store_obis_by_geo <- function(k, list_phyla) {
   res <- store_obis_by_geo()$get(k)
   phyla_to_keep <- na.omit(list_phyla$phylum[list_phyla$common_phylum != "to_drop"])
+  message("grid: ", k)
+  message(str(res))
   if (nrow(res) > 0) {
     res <- res[tolower(res$phylum) %in% phyla_to_keep &
                  (!tolower(res$class) %in% chordata_classes_to_rm()), ]
     res <- sok_as_character(res, c("recordedBy", "identifiedBy", "behavior"))
-  } else {
-    NULL
+    return(res)
   }
+  NULL
 }
 
 fill_store_obis_by_geo <- function(map_geojson, list_phyla, cellsize = .5) {
