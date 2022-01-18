631ec93cfa863711889f1e8a5331921bb3f24312
François Michonneau <francois.michonneau@gmail.com>
2022-01-18 00:59:01 -0500

    rework hook to use retries

--- R/obis.R
+++ R/obis.R
@@ -1,14 +1,28 @@
 ## key the OBIS occurrences from the AphiaID (worms_id)
 fetch_hook_obis_occurrences <- function(key, namespace) {
-  if (!is.na(key)) {
-    res <- try(robis::occurrence(taxonid = key), silent = TRUE)
-  } else {
+  if (is.na(key)) {
     return(NULL)
   }
+  .fetch_hook <- function(key) {
+    robis::occurrence(taxonid = key)
+  }
+  res <- try(.fetch_hook(key), silent = TRUE)
+  attempts <- 0
+  pred <- inherits(res, "try-error")
+  while (pred && attempts <= 3) {
+    v3("attempt ", attempts, " failed, sleeping ...")
+    Sys.sleep(exp(runif(1) * attempts))
+    res <- try(.fetch_hook(key), silent = TRUE)
+    pred <- inherits(res, "try-error")
+    attempts <- attempts + 1
+  }
+
+  if (pred) {
+    stop("failed: ", res)
+  }
+
   if (nrow(res) == 0) {
     return(NULL)
-  } else if (inherits(res, "try-error")) {
-    stop("something's wrong")
   } else {
     v2(" ... ", appendLF = FALSE)
     res
